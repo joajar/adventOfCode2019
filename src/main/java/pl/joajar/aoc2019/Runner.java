7acf363bbc3790dd5a0516b48d86dc2efcfeaac8
joajar <joanna.z.jaroszewska@gmail.com>
2019-12-31 20:12:40 +0100

    Data for day 2 puzzles displayed.

--- src/main/java/pl/joajar/aoc2019/Runner.java
+++ src/main/java/pl/joajar/aoc2019/Runner.java
@@ -1,10 +1,13 @@
 package pl.joajar.aoc2019;
 
 import pl.joajar.aoc2019.solutions.Day01;
+import pl.joajar.aoc2019.solutions.Day02;
 
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Day 1 part 1 result: " + Day01.part1result());
         System.out.println("Day 1 part 2 result: " + Day01.part2result());
+        System.out.print("Day 2 data: ");
+        Day02.printData();
     }
 }
---
+++ src/main/java/pl/joajar/aoc2019/solutions/Day02.java
@@ -0,0 +1,29 @@
+package pl.joajar.aoc2019.solutions;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Day02 {
+    public static void printData() {
+        System.out.println(Arrays.toString(getData()));
+    }
+
+    private static int[] getData() {
+        BufferedReader bufferedReader;
+        try {
+
+            bufferedReader = new BufferedReader(new FileReader(new File("src/main/java/pl/joajar/aoc2019/inputs/Day02.txt")));
+            int[] workingArray = Arrays.stream(bufferedReader.readLine().split(",", 0)).mapToInt(Integer::parseInt).toArray();
+            bufferedReader.close();
+            return workingArray;
+
+        } catch (FileNotFoundException e1) {
+            System.out.println("Do not found a file!");
+            e1.printStackTrace();
+        } catch (IOException e2) {
+            System.out.println("IO error!");
+            e2.printStackTrace();
+        }
+        return new int[0];
+    }
+}
